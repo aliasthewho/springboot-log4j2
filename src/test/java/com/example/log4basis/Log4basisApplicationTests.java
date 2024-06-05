2c95d9d166818aa209dc39c8274a422950c25f93
Miguel Alcantara <miguel.alc@gmail.com>
2024-06-05 15:30:08 -0500

    first commit

---
+++ src/test/java/com/example/log4basis/Log4basisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.log4basis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4basisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
